0b03fac8ac993fdf3d59666bfcfe06d10b53f3bb
Robin Linacre <robinlinacre@hotmail.com>
2017-06-25 22:46:45 +0100

    start body, add footer template

---
+++ R/footer.R
@@ -0,0 +1,23 @@
+footer_initialise <- function() {
+}
+
+add_footer <- function() {
+}
+
+footer_get_wb_cols <- function() {
+}
+
+footer_get_wb_rows <- function() {
+}
+
+footer_get_bottom_wb_row <- function() {
+}
+
+footer_get_rightmost_wb_col <- function() {
+}
+
+footer_get_cell_styles_table <- function() {
+}
+
+footer_write_rows <- function() {
+}
