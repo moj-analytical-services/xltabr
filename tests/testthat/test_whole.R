9d65751261183d1433e0c4fa074370601c25c043
Robin Linacre <robin.linacre@digital.justice.gov.uk>
2017-07-19 21:33:09 +0100

    update unit tests'

--- tests/testthat/test_whole.R
+++ tests/testthat/test_whole.R
@@ -49,8 +49,8 @@ test_that("Simple test of dimensions, title", {
   testthat::expect_true(t3)
 
   # Check that the cols are reported correctly
-  t1 = xltabr:::title_get_wb_cols(tab_all_elements_no_offset) ==1
-  t2 = xltabr:::title_get_wb_cols(tab_no_title) == integer(0)
+  t1 = xltabr:::title_get_wb_cols(tab_all_elements_no_offset) == 1
+  t2 = identical(xltabr:::title_get_wb_cols(tab_no_title), integer(0)) #Because you can't do integer(0) == integer(0)
   t3 = xltabr:::title_get_wb_cols(tab_all_elements_offset) == 4
 
   testthat::expect_true(t1)
@@ -59,12 +59,8 @@ test_that("Simple test of dimensions, title", {
 
   # Check that the rows are reported correctly
   t1 = xltabr:::title_get_wb_rows(tab_all_elements_no_offset) == 1:3
-  t2 = xltabr:::title_get_wb_rows(tab_no_title) == integer(0)
-  t3 = xltabr:::title_get_wb_rows(tab_all_elements_offset) == 10:13
-
-  xltabr:::title_get_wb_rows(tab_all_elements_no_offset)
-  xltabr:::title_get_wb_rows(tab_no_title)
-  xltabr:::title_get_wb_rows(tab_all_elements_offset)
+  t2 = identical(xltabr:::title_get_wb_rows(tab_no_title) ,integer(0))
+  t3 = xltabr:::title_get_wb_rows(tab_all_elements_offset) == 10:12
 
   testthat::expect_true(all(t1))
   testthat::expect_true(t2)
@@ -73,19 +69,14 @@ test_that("Simple test of dimensions, title", {
   xltabr:::title_get_bottom_wb_row(tab_all_elements_offset)
 
 
-  xltabr:::title_get_bottom_wb_row(tab)
-  xltabr:::title_get_rightmost_wb_col(tab)
-  xltabr:::title_get_wb_cols(tab)
-  xltabr:::title_get_wb_rows(tab)
-
-
-    testthat::expect_true(all(tab$body$body_df$meta_row_ == c("body", "body") ))
-  testthat::expect_true(all(tab$body$body_df$meta_left_header_row_ == c("left_header_1", "left_header_1") ))
-  testthat::expect_true(all(tab$body$meta_col_ == c("", "") ))
+  testthat::expect_true(all(tab_all_elements_no_offset$body$body_df$meta_row_ == c("body", "body", "body") ))
+  testthat::expect_true(all(tab_all_elements_no_offset$body$body_df$meta_left_header_row_ == c("left_header_1", "left_header_1", "left_header_1") ))
+  testthat::expect_true(all(tab_all_elements_no_offset$body$meta_col_ == c("", "") ))
 
   # Test styles derived correctly
-  df2 <- xltabr:::body_get_cell_styles_table(tab)
-  testthat::expect_true(all(df2$style_name == c("body|left_header_1", "body|left_header_1", "body", "body")))
+  df2 <- xltabr:::body_get_cell_styles_table(tab_all_elements_no_offset)
+  tab_all_elements_no_offset$body$body_df
+  testthat::expect_true(all(df2$style_name == c("body|left_header_1", "body|left_header_1","body|left_header_1", "body", "body", "body")))
 
 
 
