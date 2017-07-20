ee0b9181e1147edd6868473db1c27289240ea069
Karik Isichei <karik.isichei@gmail.com>
2017-07-20 17:27:36 +0100

    added basic styling functions to maintain style dictionary, new test output in test_delete_2

--- R/style_catalogue.R
+++ R/style_catalogue.R
@@ -42,3 +42,79 @@ style_catalogue_override_styles <- function(tab, path_to_xlsx) {
   tab
 }
 
+# # # # # Added functions by Karik # # # # #
+
+# # # # # # # # # # # # # # # # # # # #
+
+# add_to_dictionary
+# returns a style_key string based on our style catelogue objects (should also work with default R lists)
+create_style_key <- function(sc){
+
+  # NULL returns from property_to_key are automatically removed from vector
+  style_properties <- c(property_to_key(sc$style, "fontName"),
+                        property_to_key(sc$style, "fontSize"),
+                        property_to_key(sc$style, "fontColour"),
+                        property_to_key(sc$style, "numFmt"),
+                        property_to_key(sc$style, "border"),
+                        property_to_key(sc$style, "borderColour"),
+                        property_to_key(sc$style, "bordersc$style"),
+                        property_to_key(sc$style, "bgFill"),
+                        property_to_key(sc$style, "fgFill"),
+                        property_to_key(sc$style, "halign"),
+                        property_to_key(sc$style, "valign"),
+                        property_to_key(sc$style, "fontDecoration"),
+                        property_to_key(sc$style, "wrapText"),
+                        property_to_key(sc$style, "textRotation"),
+                        property_to_key(sc$style, "indent"),
+                        paste0("rowHeight_", sc$row_height))
+
+  style_key <- paste(style_properties, collapse = "|")
+
+  style_key
+}
+
+# Converts style object property to string representation (used in create_style_key function)
+property_to_key <- function(style_object, property){
+  if(is.null(style_object[[property]])){
+    return(NULL)
+  }
+  else{
+    return(paste(property, paste0(style_object[[property]], collapse = "%"), sep = "_"))
+  }
+}
+
+# Converts style_key to style_list
+style_key_passer <- function(style_key){
+  specs <- unlist(strsplit(style_key, "\\|"))
+
+  # utilising Rs stupid ordered hashing for the first and probably last time.
+  # Can interate through style categories in order to get inherited changes from left to right.
+  style_categories <- list()
+  for (i in 1:length(specs)){
+    seperate_property <- unlist(strsplit(specs[[i]], "_"))
+    style_categories[seperate_property[1]] <- unlist(strsplit(seperate_property[2], "%"))
+  }
+
+  style_categories
+}
+
+# looks at the cell_style_definition and build a final style_key for that cell
+build_style <- function(cell_style_definition){
+  # Convert cell style inheritence string into an array
+  seperated_style_definition <- unlist(strsplit(cell_style_definition, "\\|"))
+  if (length(seperated_style_definition) <= 1){
+    # If only one style (e.g body then return it's style_key)
+    return (style_dictionary[[style]])
+  }
+  else{
+    # Otherwise build final style
+    previous_style <- style_key_passer(style_dictionary[[seperated_style_definition[1]]])
+    for (i in 2:length(seperated_style_definition)){
+      current_style <- style_key_passer(style_dictionary[[seperated_style_definition[i]]])
+      for (property_name in names(current_style)){
+        previous_style[property_name] <- current_style[property_name]
+      }
+    }
+  }
+  return (create_style_key(previous_style))
+}
--- test_delete_2.R
+++ test_delete_2.R
@@ -25,3 +25,40 @@ xltabr:::body_write_rows(tab)
 xltabr:::footer_write_rows(tab)
 
 openxlsx::openXL(tab$wb)
+
+
+r1 <- paste0("r1_col_", as.character(1:3))
+r2 <- paste0("r2_col_", as.character(1:3))
+h_list <- list( r1,  r2)
+
+tab <- xltabr::initialise() %>%
+  xltabr::add_title(title_text, title_style_names) %>%
+  xltabr::add_top_headers(h_list, col_style_names = c("c1", "c2", "c3"), row_style_names = c("r1", "r2"))
+
+xltabr:::title_write_rows(tab)
+xltabr:::top_headers_write_rows(tab)
+
+xltabr:::title_get_cell_styles_table(tab)
+xltabr:::top_headers_get_cell_styles_table(tab)
+
+
+### TESTING APPLYING STYLES
+title_cells <- xltabr:::title_get_cell_styles_table(tab)
+unique_styles <- title_cells %>%
+  dplyr::select(style_name) %>%
+  dplyr::distinct()
+unique_styles <- as.character(unique_styles[,1])
+
+
+for (style in unique_styles){
+  row_col_styles <- title_cells %>% dplyr::filter(style_name == style)
+  rows <- row_col_styles$row
+  cols <- row_col_styles$col
+
+  openxlsx::addStyle(tab$wb, tab$wb$sheet_names[1], tab$style_catalogue[[tab$style_dictionary[[style]]]]$style, rows, cols)
+}
+
+names(tab$style_catalogue)
+names(tab$style_dictionary)
+
+openxlsx::saveWorkbook(tab$wb, "testoutput.xlsx")
