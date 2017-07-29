a7ad0f809e6b4e52ca67f3cc6f657148cd18a9a6
Robin Linacre <robin.linacre@digital.justice.gov.uk>
2017-07-29 14:02:58 +0100

    added null tests

--- tests/testthat/test_whole.R
+++ tests/testthat/test_whole.R
@@ -24,7 +24,8 @@ tab_all_elements_no_offset <- xltabr::initialise() %>%
 
 tab_no_title <- xltabr::initialise() %>%
   xltabr::add_top_headers(h_list) %>%
-  xltabr::add_body(df, left_header_colnames = "a")
+  xltabr::add_body(df, left_header_colnames = "a") %>%
+  xltabr::add_footer(footer_text, footer_style_names)
 
 tab_all_elements_offset <- xltabr::initialise(topleft_row = 10, topleft_col  = 4) %>%
   xltabr::add_title(title_text, title_style_names) %>%
@@ -159,6 +160,86 @@ test_that("Simple test of dimensions, body", {
   testthat::expect_true(all(t1))
   testthat::expect_true(t2)
   testthat::expect_true(all(t3))
+
+})
+
+
+test_that("Simple test of dimensions, footer", {
+
+  # Check bottom row is reported correctly
+  t1 = xltabr:::footer_get_bottom_wb_row(tab_all_elements_no_offset) == 10
+  t2 = xltabr:::footer_get_bottom_wb_row(tab_no_title) == 7
+  t3 = xltabr:::footer_get_bottom_wb_row(tab_all_elements_offset) == 19
+
+  testthat::expect_true(t1)
+  testthat::expect_true(t2)
+  testthat::expect_true(t3)
+
+  # Check the rightmost column is reported correctly
+  t1 = xltabr:::footer_get_rightmost_wb_col(tab_all_elements_no_offset) == 1
+  t2 = xltabr:::footer_get_rightmost_wb_col(tab_no_top_headers) == 1
+  t3 = xltabr:::footer_get_rightmost_wb_col(tab_all_elements_offset) == 4
+
+  testthat::expect_true(t1)
+  testthat::expect_true(t2)
+  testthat::expect_true(t3)
+
+  # Check that the cols are reported correctly
+  t1 = all(xltabr:::footer_get_wb_cols(tab_all_elements_no_offset) == 1)
+  t2 = all(xltabr:::footer_get_wb_cols(tab_no_top_headers) == 1)
+  t3 = all(xltabr:::footer_get_wb_cols(tab_all_elements_offset) == 4)
+
+  testthat::expect_true(t1)
+  testthat::expect_true(t2)
+  testthat::expect_true(t3)
+
+  # Check that the rows are reported correctly
+  t1 = all(xltabr:::footer_get_wb_rows(tab_all_elements_no_offset) == 9:10)
+  t2 = all(xltabr:::footer_get_wb_rows(tab_no_top_headers) == 7:8)
+  t3 = xltabr:::footer_get_wb_rows(tab_all_elements_offset) == 18:19
+
+  testthat::expect_true(all(t1))
+  testthat::expect_true(t2)
+  testthat::expect_true(all(t3))
+
+})
+
+test_that("Test of combined dimension", {
+
+  t1 = all(xltabr:::extent_get_rows(tab_all_elements_no_offset) == 1:10)
+  t2 = all(xltabr:::extent_get_rows(tab_no_top_headers) == 1:8)
+  t3 = all(xltabr:::extent_get_rows(tab_all_elements_offset) == 10:19)
+
+  testthat::expect_true(t1)
+  testthat::expect_true(t2)
+  testthat::expect_true(t3)
+
+  t1 = all(xltabr:::extent_get_cols(tab_all_elements_no_offset) == 1:2)
+  t2 = all(xltabr:::extent_get_cols(tab_no_top_headers) == 1:2)
+  t3 = all(xltabr:::extent_get_cols(tab_all_elements_offset) == 4:5)
+
+  testthat::expect_true(t1)
+  testthat::expect_true(t2)
+  testthat::expect_true(t3)
+
+
+  t1 = all(xltabr:::extent_get_bottom_wb_row(tab_all_elements_no_offset) == 10)
+  t2 = all(xltabr:::extent_get_bottom_wb_row(tab_no_top_headers) == 8)
+  t3 = all(xltabr:::extent_get_bottom_wb_row(tab_all_elements_offset) == 19)
+
+  testthat::expect_true(t1)
+  testthat::expect_true(t2)
+  testthat::expect_true(t3)
+
+
+  t1 = all(xltabr:::extent_get_rightmost_wb_col(tab_all_elements_no_offset) == 2)
+  t2 = all(xltabr:::extent_get_rightmost_wb_col(tab_no_top_headers) == 2)
+  t3 = all(xltabr:::extent_get_rightmost_wb_col(tab_all_elements_offset) == 5)
+
+  testthat::expect_true(t1)
+  testthat::expect_true(t2)
+  testthat::expect_true(t3)
+
 
 })
 
