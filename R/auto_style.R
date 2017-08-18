3ddf33922207b2e4fdc49381a81264a1709c5bb5
Robin Linacre <robin.linacre@digital.justice.gov.uk>
2017-08-18 16:21:16 +0100

    updated to make indent names more flexible

--- R/auto_style.R
+++ R/auto_style.R
@@ -43,8 +43,6 @@ auto_style_number_formatting <- function(tab, overrides = list()) {
 
   }
 
-
-
   tab
 
 }
@@ -62,7 +60,6 @@ auto_style_body_rows <- function(tab, indent = FALSE, keyword = "(all)") {
 
   # Autodetect the 'summary level' e.g. header 1 is most prominent, header 2 next etc.
   tab <- auto_detect_body_title_level(tab, keyword)
-
 
   tab
 
@@ -108,14 +105,22 @@ auto_detect_left_headers <- function(tab, keyword = "(all)") {
 }
 
 # For title level
+# Note we assume there are a maximum of 5 title levels
 get_inv_title_count_title <- function(left_headers_df, keyword) {
-
+  # +-------+-------+-------+-----------+-------------+--------------+
+  # | col1  | col2  | col3  | all_count | title_level | indent_level |
+  # +-------+-------+-------+-----------+-------------+--------------+
+  # | (all) | (all) | (all) |         3 | title_3     |              |
+  # | (all) | (all) | -     |         2 | title_4     | indent_1     |
+  # | (all) | -     | -     |         1 | title_5     | indent_2     |
+  # | -     | -     | -     |         0 |             | indent_3     |
+  # +-------+-------+-------+-----------+-------------+--------------+
   to_count <- (left_headers_df == keyword)
   all_count <- rowSums(to_count)
 
   #rows with higher (all) count should have a lower title value because title_1 is the most emphasized
-  all_count_inv <- max(all_count) - all_count + 1
-  all_count_inv[all_count == 0] <- NA
+  all_count_inv <- 6 - all_count
+  all_count_inv[all_count_inv == 6] <- NA
 
   all_count_inv
 
@@ -123,12 +128,21 @@ get_inv_title_count_title <- function(left_headers_df, keyword) {
 
 # For indent level
 get_inv_title_count_indent <- function(left_headers_df, keyword) {
+  # +-------+-------+-------+-----------+-------------+--------------+
+  # | col1  | col2  | col3  | all_count | title_level | indent_level |
+  # +-------+-------+-------+-----------+-------------+--------------+
+  # | (all) | (all) | (all) |         3 | title_3     |              |
+  # | (all) | (all) | -     |         2 | title_4     | indent_1     |
+  # | (all) | -     | -     |         1 | title_5     | indent_2     |
+  # | -     | -     | -     |         0 |             | indent_3     |
+  # +-------+-------+-------+-----------+-------------+--------------+
 
   to_count <- (left_headers_df == keyword)
   all_count <- rowSums(to_count)
 
   #rows with higher (all) count should have a lower title value because title_1 is the most emphasized
   all_count_inv <- max(all_count) - all_count
+  all_count_inv[all_count_inv == 0] <- NA
 
   all_count_inv
 
@@ -159,8 +173,9 @@ auto_detect_body_title_level <- function(tab, keyword = "(all)") {
 # Consolidate the header columns into one, taking the rightmost value and applying indent
 # e.g. a | b | (all) -> b
 # e.g. (all) | (all) | (all) -> Grand Total
-auto_style_indent <- function(tab, keyword = "(all)", total_text = "Grand Total") {
+auto_style_indent <- function(tab, keyword = "(all)", total_text = "Grand Total", left_header_colname = "-") {
 
+  tab$misc$coalesce_left_header_colname = left_header_colname
   if (is.null(tab$body$left_header_colnames )) {
     Stop("You've called auto_style_indent, but there are no left_header_colnames to work with")
   }
@@ -175,7 +190,6 @@ auto_style_indent <- function(tab, keyword = "(all)", total_text = "Grand Total"
 
   # paste together all left headers
   concat <- do.call(paste, c(left_headers_df, sep="=|="))
-  # concat <- gsub("\\(all\\)","", concat)
 
   # Split concatenated string into elements, and find last element that's not (all)
   elems <- strsplit(concat, "=\\|=", perl=TRUE)
@@ -194,20 +208,26 @@ auto_style_indent <- function(tab, keyword = "(all)", total_text = "Grand Total"
   cols <- !(names(tab$body$body_df_to_write) %in% tab$body$left_header_colnames)
   tab$body$body_df_to_write <- tab$body$body_df_to_write[cols]
   tab$body$body_df_to_write  <- cbind(new_left_headers = new_left_headers, tab$body$body_df_to_write, stringsAsFactors = FALSE)
+  names(tab$body$body_df_to_write)[1] <- left_header_colname
 
-  tab$body$left_header_colnames <- c("new_left_headers")
+
+
+  tab$body$left_header_colnames <- c(left_header_colname)
 
   # Now need to fix meta_left_header_row_ to include indents
   #Set meta_left_header_row_ to include relevant indents
   all_count_inv <- get_inv_title_count_indent(left_headers_df, keyword = keyword)
 
   col <- tab$body$body_df$meta_left_header_row_
-  concat <- all_count_inv[not_na(all_count_inv)]
+  rows_indices_to_change <- not_na(all_count_inv)
+  concat <- all_count_inv[rows_indices_to_change]
   concat <- paste0("indent_", concat)
-  tab$body$body_df[not_na(all_count_inv),"meta_left_header_row_"] <- paste(col, concat,sep = "|")
+
+  tab$body$body_df[rows_indices_to_change,"meta_left_header_row_"] <- paste(col[rows_indices_to_change], concat,sep = "|")
 
   # Update body$meta_col_
-  tab$body$meta_col_ <- c("new_left_headers" = tab$body$meta_col_[[1]], tab$body$meta_col_[cols])
+  tab$body$meta_col_ <- c(left_header_colname = tab$body$meta_col_[[1]], tab$body$meta_col_[cols])
+  names(tab$body$meta_col_)[1] <- left_header_colname
 
   # Finally, if tab$top_headers has too many cols, remove the extra cols, removing from 2:n
   if (not_null(tab$top_headers$top_headers_list)) {
@@ -221,10 +241,11 @@ auto_style_indent <- function(tab, keyword = "(all)", total_text = "Grand Total"
       for (r in length(tab$top_headers$top_headers_list)) {
 
         tab$top_headers$top_headers_list[[r]] <- tab$top_headers$top_headers_list[[r]] [cols_to_retain]
-        tab$top_headers$top_headers_list[[r]][1] <- "new_left_headers"
+        tab$top_headers$top_headers_list[[r]][1] <- left_header_colname
       }
 
     }
   }
+
   tab
 }
