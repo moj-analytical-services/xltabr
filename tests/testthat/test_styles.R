35acda92b48e1005a24a6a88e16145d06359dded
Karik Isichei <karik.isichei@justice.gsi.gov.uk>
2017-08-25 12:38:14 +0000

    style testing

---
+++ tests/testthat/test_styles.R
@@ -0,0 +1,15 @@
+test_that("style conversion functions work as expected", {
+
+  style_key_test <- "fontName_Calibri|fontSize_12|textDecoration_BOLD%ITALIC"
+
+  t1 <- xltabr:::style_key_parser(style_key_test)
+
+  expect_true(all(names(t1) == c("fontName", "fontSize", "textDecoration")))
+  expect_true(t1[["fontName"]] == "Calibri")
+  expect_true(t1[["fontSize"]] == "12")
+  expect_true(all(t1[["textDecoration"]] == c("BOLD","ITALIC")))
+
+  expect_true(xltabr:::create_style_key(t1) == style_key_test)
+
+
+})
