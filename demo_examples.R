33e18b4fc92282cbc0e64a96df58bf93e409f9c8
Robin Linacre <robin.linacre@digital.justice.gov.uk>
2017-08-25 17:26:04 +0100

    updated readme

--- demo_examples.R
+++ demo_examples.R
@@ -4,16 +4,19 @@ library(dplyr)
 # Read data from main database and run cross tabulation
 path <- system.file("extdata", "synthetic_data.csv", package="xltabr")
 df <- read.csv(path, stringsAsFactors = FALSE)
-ct <- reshape2::dcast(df, drive + age + colour + type ~ date, value.var= "value", margins=c("drive", "age", "colour", "type"), fun.aggregate = sum)
+ct <- reshape2::dcast(df, drive + age  ~ type, value.var= "value", margins=c("drive", "age"), fun.aggregate = sum)
 ct <- ct %>% dplyr::arrange(-row_number())
 
 
 # Example 1: default settings
-tab <- xltabr::auto_crosstab_to_wb(ct, titles = "This is the title", return_tab = TRUE)
-openxlsx::openXL(tab$wb)
+titles = c("Breakdown of car statistics", "Cross tabulation of drive and age against type*", "")
+footers = "*age as of January 2015"
+wb <- xltabr::auto_crosstab_to_wb(ct, titles = titles, footers = footers)
+openxlsx::openXL(wb)
+
+tab$style_catalogue$footer
 
 # Example 2:  User provides their own formatting options
-
 path <- system.file("extdata", "styles_pub.xlsx", package = "xltabr")
 num_path <- system.file("extdata", "style_to_excel_number_format_alt.csv", package = "xltabr")
 title <- "This is the title"
