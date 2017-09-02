01500e90afc26561a3c4b928ca141988c4dd360f
Robin Linacre <robinlinacre@hotmail.com>
2017-09-02 08:42:44 +0100

    fixed examples

--- R/convenience.R
+++ R/convenience.R
@@ -13,7 +13,7 @@
 #' @param auto_merge Boolean.  Whether to merge cells in the title and footers to width of body
 #' @param insert_below_tab. A existing tab object.  If provided, this table will be written on the same sheet, below the provided tab.
 #'
-#' @examples auto_df_to_xl(mtcars, titles="the mtcars data", footers="note, this data is now quite old", auto_open=TRUE, auto_number_format = FALSE)
+#' @examples auto_df_to_wb(mtcars, titles="the mtcars data", footers="note, this data is now quite old", auto_open=TRUE, auto_number_format = FALSE)
 #' @export
 auto_df_to_wb <-
   function(df,
@@ -116,7 +116,7 @@ auto_crosstab_to_wb <-
            total_text = NULL,
            include_header_rows = TRUE,
            wb = NULL,
-           ws_name = NULL, 
+           ws_name = NULL,
            number_format_overrides = list()) {
 
   top_header_provided <- TRUE
