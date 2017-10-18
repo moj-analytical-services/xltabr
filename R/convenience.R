f6d2ba3548175c10b2706d4f7cbf515d2f0d7454
Robin Linacre <robin.linacre@digital.justice.gov.uk>
2017-10-18 18:01:29 +0100

    enabled customisation of auto tab

--- R/convenience.R
+++ R/convenience.R
@@ -120,51 +120,24 @@ auto_crosstab_to_wb <-
            fill_non_values_with = list(na = NULL, nan = NULL, inf = NULL, neg_inf = NULL),
            allcount_to_level_translate = NULL) {
 
-  top_header_provided <- TRUE
-  if (is.null(top_headers)) {
-    top_header_provided <- FALSE
-    top_headers  <- names(df)
-  }
 
-
-  tab <- initialise(insert_below_tab = insert_below_tab, wb = wb, ws_name = ws_name)
-
-  if (include_header_rows) {
-    tab <- add_top_headers(tab, top_headers)
-  }
-
-  tab <- add_body(tab, df,left_header_colnames = left_header_colnames, fill_non_values_with = fill_non_values_with)
-
-  if (is.null(left_header_colnames)) {
-    tab <- auto_detect_left_headers(tab)
-  }
-
-  tab <- auto_detect_body_title_level(tab, allcount_to_level_translate = allcount_to_level_translate)
-
-  if (auto_number_format) {
-    tab <- auto_style_number_formatting(tab, overrides = number_format_overrides)
-  }
-
-  if (not_null(titles)) {
-    tab <- add_title(tab, titles)
-  }
-
-  if (indent) {
-    tab <- auto_style_indent(tab, total_text = total_text)
-  }
-
-  if (vertical_border) {
-    tab <- add_left_header_vertical_border(tab)
-  }
-
-  if (not_null(footers)) {
-    tab <- add_footer(tab, footers)
-  }
-
-  if (auto_merge) {
-    tab <- auto_merge_title_cells(tab)
-    tab <- auto_merge_footer_cells(tab)
-  }
+  tab <- auto_crosstab_to_tab(df,
+                       auto_number_format = auto_number_format,
+                       top_headers = top_headers,
+                       titles = titles,
+                       footers = footers,
+                       indent = indent,
+                       left_header_colnames = left_header_colnames,
+                       vertical_border = vertical_border,
+                       auto_merge = auto_merge,
+                       insert_below_tab = insert_below_tab,
+                       total_text = total_text,
+                       include_header_rows = include_header_rows,
+                       wb = wb,
+                       ws_name = ws_name,
+                       number_format_overrides = number_format_overrides,
+                       fill_non_values_with = fill_non_values_with,
+                       allcount_to_level_translate = allcount_to_level_translate)
 
   tab <- write_all_elements_to_wb(tab)
 
@@ -181,4 +154,101 @@ auto_crosstab_to_wb <-
   }
 
 
-}
+  }
+
+
+
+#' Take a cross tabulation produced by `reshape2::dcast` and output a formatted openxlsx wb object
+#'
+#' @param df A data.frame.  The cross tabulation to convert to Excel
+#' @param auto_number_format Whether to automatically detect number format
+#' @param top_headers A list.  Custom top headers. See [add_top_headers()]
+#' @param titles The title.  A character vector.  One element per row of title
+#' @param footers Table footers.  A character vector.  One element per row of footer.
+#' @param indent Automatically detect level of indentation of each row
+#' @param left_header_colnames  The names of the columns that you want to designate as left headers
+#' @param vertical_border Boolean. Do you want a left border?
+#' @param auto_merge Boolean.  Whether to merge cells in the title and footers to width of body
+#' @param insert_below_tab A existing tab object.  If provided, this table will be written on the same sheet, below the provided tab.
+#' @param total_text  The text that is used for the 'grand total' of a cross tabulation
+#' @param include_header_rows  Boolean - whether to include or omit the header rows
+#' @param number_format_overrides e.g. list("colname1" = "currency1") see [auto_style_number_formatting]
+#' @param wb A existing openxlsx workbook.  If not provided, a new one will be created
+#' @param ws_name The name of the worksheet you want to write to
+#' @param fill_non_values_with Manually specify a list of strings that will replace non numbers types NA, NaN, Inf and -Inf. e.g. list(na = '*', nan = '', inf = '-', neg_inf = '--'). Note: NaNs are not treated as NAs.
+#' @param allcount_to_level_translate Manually specify how to translate summary levels into header formatting
+#'
+#' @export
+auto_crosstab_to_tab <-
+  function(df,
+           auto_number_format = TRUE,
+           top_headers = NULL,
+           titles = NULL,
+           footers = NULL,
+           indent = TRUE,
+           left_header_colnames = NULL,
+           vertical_border = TRUE,
+           auto_merge = TRUE,
+           insert_below_tab = NULL,
+           total_text = NULL,
+           include_header_rows = TRUE,
+           wb = NULL,
+           ws_name = NULL,
+           number_format_overrides = list(),
+           fill_non_values_with = list(na = NULL, nan = NULL, inf = NULL, neg_inf = NULL),
+           allcount_to_level_translate = NULL) {
+
+    top_header_provided <- TRUE
+    if (is.null(top_headers)) {
+      top_header_provided <- FALSE
+      top_headers  <- names(df)
+    }
+
+
+    tab <- initialise(insert_below_tab = insert_below_tab, wb = wb, ws_name = ws_name)
+
+    if (include_header_rows) {
+      tab <- add_top_headers(tab, top_headers)
+    }
+
+    tab <- add_body(tab, df,left_header_colnames = left_header_colnames, fill_non_values_with = fill_non_values_with)
+
+    if (is.null(left_header_colnames)) {
+      tab <- auto_detect_left_headers(tab)
+    }
+
+    tab <- auto_detect_body_title_level(tab, allcount_to_level_translate = allcount_to_level_translate)
+
+    if (auto_number_format) {
+      tab <- auto_style_number_formatting(tab, overrides = number_format_overrides)
+    }
+
+    if (not_null(titles)) {
+      tab <- add_title(tab, titles)
+    }
+
+    if (indent) {
+      tab <- auto_style_indent(tab, total_text = total_text)
+    }
+
+    if (vertical_border) {
+      tab <- add_left_header_vertical_border(tab)
+    }
+
+    if (not_null(footers)) {
+      tab <- add_footer(tab, footers)
+    }
+
+    if (auto_merge) {
+      tab <- auto_merge_title_cells(tab)
+      tab <- auto_merge_footer_cells(tab)
+    }
+
+    tab
+
+
+  }
+
+
+
+
