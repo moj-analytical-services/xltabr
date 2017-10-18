bbe31239f4f62bc2b780add1f839db6e0ef4145f
Robin Linacre <robin.linacre@digital.justice.gov.uk>
2017-10-18 09:57:10 +0100

    refactored style catalogue

--- R/style_catalogue.R
+++ R/style_catalogue.R
@@ -1,276 +1,273 @@
-# Use default .xlsx style catalogue to initialise the catalogue of styles
-# Following initialisation, we have tab$style catalogue, which contains each style.
-# Values are chracter 'structures' e.g. '"structure(list(valign = \"center\"), .Names = \"valign\")"'
+# Xltabr uses the concept of an 'xltabr_style'.  This contains
+# all the information needed to apply styles to cells
+# An xltabr_style is a list with several keys, as follows
+#   $style_list: An R list containing the properties that define the style, derived from openxlsx_style$style$as.list()
+#   $s4style: An openxlsx style ready to be applied to some cells - this is an openxlsx_style$style
+#   $style_string: A text string containing the name of the style e.g. body|indent_1
+#   $row_height:  The row height of this style.
+#   $excel_num_format:  The excel number format as a string
+
+# Use .xlsx file containing styles to create the style catalogue, which is a list of xltabr_style s
 style_catalogue_initialise <- function(tab) {
 
-  tab <- style_catalogue_xlsx_import(tab)
-  tab <- style_catalogue_import_num_formats(tab)
+  path <- "styles.xlsx"
+  num_path <- "style_to_excel_number_format.csv"
+  xltabr_style_lookup <- convert_xlsx_and_csv_to_style_lookup(tab)
+  xltabr_style_lookup <- add_num_formats_to_style_lookup(xltabr_style_lookup)
+  tab$style_catalogue <- xltabr_style_lookup
   tab
 }
 
-# Number formats are stored in a csv, by default at xltabr/inst/extdata/style_to_excel_number_format.csv
-# This is a named style (e.g. percent1) whcich translates into an Excel style (e.g. #,0.0%;#,0.0%;;* @)
-style_catalogue_import_num_formats <- function(tab){
+convert_xlsx_and_csv_to_style_lookup <- function(tab) {
 
-  lookup_df <- utils::read.csv(get_cell_format_path(), stringsAsFactors = FALSE)
+  # This is a bit tricky due to the potential for styles in the .xlsx file which
+  # could have no styling ( e.g. body), and therefore do not exist in when we iterate through wb$styleObjects
+  get_style_string_to_style_lookup <- function(wb) {
+
+    # Create a list whose values are xltabr_styles.  Keys are cells references (row,column)
+    xltabr_style_lookup <- list()
+    for (openxlsx_style in wb$styleObjects) {
+      for (row in openxlsx_style$rows) {
+        for (col in openxlsx_style$cols) {
+          key <-  paste(row,col, sep = ",")
+          xltabr_style_lookup[[key]] <- list()
+          xltabr_style_lookup[[key]]$style_list <- convert_s4style_to_style_list(openxlsx_style$style)
+          xltabr_style_lookup[[key]]$s4style <- openxlsx_style$style
+        }
+      }
+    }
+
+    worksheet <- wb$worksheets[[1]] # We only expect styles in the first sheet of the .xlsx workbook
+    # Associate a default style with rows in the xlsx that do not have a style
+    for (row_index in worksheet$sheet_data$rows) {
+
+      key <- paste(row_index,1, sep = ",")
+
+      if (!key %in% names(xltabr_style_lookup)) {
+        default_openxlsx_style <- openxlsx::createStyle()
+        xltabr_style_lookup[[key]] <- list()
+        xltabr_style_lookup[[key]]$style_list <- convert_s4style_to_style_list(default_openxlsx_style)
+        xltabr_style_lookup[[key]]$s4style <- default_openxlsx_style
+      }
+    }
+
+    # Finally convert the key of the lookup to the style name in the Excel cell rather than a row,column reference
+    xltabr_style_lookup_new <- list()
+    for (row_index in worksheet$sheet_data$rows) {
+      old_key <- paste(row_index,1, sep = ",")
+      value_sheet <- openxlsx::readWorkbook(wb, rows = row_index, cols = 1, colNames = FALSE)
+      new_key <- value_sheet[[1,1]]
+      xltabr_style_lookup_new[[new_key]] <- xltabr_style_lookup[[old_key]]
+      xltabr_style_lookup_new[[new_key]]$row_in_xlsx_stylesheet <- row_index
+      xltabr_style_lookup_new[[new_key]]$style_string <- new_key
+      xltabr_style_lookup_new[[new_key]]['excel_num_format'] <- list(NULL)
+    }
+
+    xltabr_style_lookup_new
+  }
+
+  # Row height is a property of the row, not in a style list, so needs to be dealt with separately
+  add_row_height_to_style_string_lookup <- function(wb, xltabr_style_lookup) {
+
+    new_xltabr_style_lookup <- list()
+
+    for (xltabr_style in xltabr_style_lookup) {
+
+      worksheet_index <- which(tab$wb$sheet_names == tab$misc$ws_name)
+      row <- as.character(xltabr_style$row_in_xlsx_stylesheet)
+      row_height <- wb$rowHeights[[1]][row]  #This will return na if there's no entry
+
+      if (is.na(row_height)) {
+        row_height <- list(NULL)
+      } else {
+        row_height <- list(as.integer(row_height))
+      }
+
+      xltabr_style["row_height"] <- row_height  #must be [] due to non equivalence of l['a'] <- list(NULL) and l[['a']] <- list(NULL)
+      new_xltabr_style_lookup[[xltabr_style$style_string]] <- xltabr_style
+    }
+
+    new_xltabr_style_lookup
+  }
+
+  wb <- openxlsx::loadWorkbook(get_style_path())
+  xltabr_style_lookup <- get_style_string_to_style_lookup(wb)
+  xltabr_style_lookup <- add_row_height_to_style_string_lookup(wb, xltabr_style_lookup)
+  xltabr_style_lookup
+
+}
+
+add_num_formats_to_style_lookup <- function(xltabr_style_lookup) {
+
+  num_formats_df <- utils::read.csv(get_cell_format_path(), stringsAsFactors = FALSE)
 
   # Convert dataframe into two vectors
-  style_keys <- lookup_df$excel_format
-  style_names <- lookup_df$style_name
+  excel_formats <- num_formats_df$excel_format
+  style_strings <- num_formats_df$style_name
 
-  if (length(style_names) == 0) {
+  if (length(style_strings) == 0) {
     warning("Your number format excel contained no data.  For an example see xltabr/inst/extdata/style_to_excel_number_format.csv")
     return(tab)
   }
 
-  for (i in 1:length(style_names)) {
-    tab$style_catalogue[[style_names[i]]] <- convert_style_list_to_character_key(list(numFmt = style_keys[i]))
-  }
-  tab
-}
+  for (i in 1:length(style_strings)) {
+    style_string <- style_strings[i]
+    s4style <- openxlsx::createStyle(numFmt = excel_formats[[i]])
 
-# Iterate through the default style workbook, adding them to the styles catalogue
-style_catalogue_xlsx_import <- function(tab) {
-
-  path <- get_style_path()
-
-  wb <- openxlsx::loadWorkbook(path)
-
-  # When you load an Excel document, openxlsx creates a list of the distinct style objects in the workbook
-  # Iterate through these style objects
-  for (this_style_object in wb$styleObjects) {
-
-    # It's possible that two styles (e.g. top header, title1) might have the same style.  If so, create an entry for both
-    for (this_row in 1:length(this_style_object$rows)) {
-
-      r <- this_style_object$rows[this_row]
-      c <- 1 # We assume that all styles are stored in the leftmost column
-
-      suppressWarnings(cell <- openxlsx::readWorkbook(wb, rows = r, cols = c, colNames = FALSE, rowNames = FALSE))
-
-      style_cat_name <- cell[1, 1]
-
-      if (is_null_or_blank(style_cat_name)) {
-        next
-      }
-
-      #See https://github.com/moj-analytical-services/xltabr/issues/88
-      style_cat_name <- as.character(style_cat_name)
-
-      # Convert from s4 object to list
-      style_list <- convert_style_object_to_list(this_style_object$style)
-
-      # Add row height property if it's different to the default
-      if (as.character(r) %in% names(wb$rowHeights[[1]])) {
-        style_list$row_height <- wb$rowHeights[[1]][[as.character(r)]]
-        style_list$row_height <- as.double(style_list$row_height)
-      }
-
-      #Convert from list to charaster string
-      style_key <- convert_style_list_to_character_key(style_list)
-
-      tab$style_catalogue[[style_cat_name]] <- style_key
-    }
+    xltabr_style_lookup[[style_string]] <- list()
+    xltabr_style_lookup[[style_string]]$style_list <- convert_s4style_to_style_list(s4style)
+    xltabr_style_lookup[[style_string]]$excel_num_format <- excel_formats[[i]]
+    xltabr_style_lookup[[style_string]]$style_list$numFmt <- NULL
+    xltabr_style_lookup[[style_string]]$s4style <- s4style
+    xltabr_style_lookup[[style_string]]["row_height"] <- list(NULL)
+    xltabr_style_lookup[[style_string]]$style_string <- style_string
   }
 
-  # If there are any cells populated with text (a style name) but that do not have any styling applied
-  # These need to be entered into the style catalogue with the default style
-  # Otherwise, when xltabr tries to apply their styling, it will raise a 'style not found' error
-
-  # Listed styles is a table with all the cells with contents in the styles workbook
-  listed_styles <- openxlsx::readWorkbook(wb, colNames = FALSE)
-  for (style_name in listed_styles$X1) {
-    if (!style_name %in% names(tab$style_catalogue)) {
-      tab$style_catalogue[[style_name]] <- convert_style_list_to_character_key(convert_style_object_to_list(openxlsx::createStyle()))
-    }
-  }
-
-  tab
-
+  xltabr_style_lookup
 
 }
 
-# Serialises a list to a character string
-# Returns a style_key string based on our style catalogue objects (should also work with default R lists)
-convert_style_list_to_character_key <- function(style_list){
-  style_key <- gsub(' +', ' ', paste0(utils::capture.output(dput(style_list)), collapse = ""))
-  style_key
-}
+convert_s4style_to_style_list <- function(s4style) {
 
-# Parses a character style_key to style_list
-style_key_parser <- function(style_key){
-  style_list <- eval(parse(text = style_key))
+  style_list <- s4style$as.list()
+  # For some reason fills do not export properly so workaround is added here
+  if (any(c("fillFg", "fillBg") %in% names(style_list))) {
+    style_list[["fill"]] <- list(fillFg = style_list[["fillFg"]], fillBg = style_list[["fillBg"]])
+    style_list[["fillFg"]] <- NULL
+    style_list[["fillBg"]] <- NULL
+  }
+
   style_list
-}
-
-# Where we have an existing style, and a new style, this function determines the inheritance rules
-# similar to cascading style sheets
-# e.g. in body|header_1, we want header_1 styles to over
-cascade_style <- function(existing_style, new_style) {
-
-  for (property_name in names(new_style)) {
-
-    # fontDectoration is a special case because we we want bold + italic to = bolditalic, not italic
-    if (property_name == "fontDecoration") {
-      existing_style[[property_name]] <- unique(c(existing_style[[property_name]], new_style[[property_name]]))
-    } else {
-      existing_style[property_name] <- new_style[property_name]
-    }
-  }
-
-  existing_style
 
 }
 
-# looks at the cell_style_definition and build a final style_key for that cell
-build_style <- function(tab, cell_style_definition){
+# Style string is like "body|header_1"
+style_string_parse_and_combine <- function(style_lookup, style_string) {
 
-  # Convert cell style inheritence string into an array
-  seperated_style_definition <- unlist(strsplit(cell_style_definition, "\\|"))
+  components <- strsplit(style_string, "\\|")[[1]]
 
-  ## Run a check (that all base styles referenced in cell_style_definition exist in style_catalogue)
-  # Get array of base styles (i.e. styles that are not a combination of multiple styles (no pipes in names))
-  base_styles <- names(tab$style_catalogue)[!grepl("\\|",names(tab$style_catalogue))]
-  ussd <- unique(seperated_style_definition)
-  style_check <- ussd %in% base_styles
-  if (!all(style_check)) {
-    stop(paste("The following style names:",  paste0(ussd[!style_check], collapse = ", "), "are not in the style_catalogue please add then maunally or specify them in style.xlsx"))
+  xltabr_styles_to_combine <- list()
+
+  for (c in components) {
+    xltabr_styles_to_combine[[c]] <- style_lookup[[c]]
   }
 
+  # This is a reduce operation, but Reduce only works with vectors
+  base_style <- xltabr_styles_to_combine[[1]]
+  for (s in xltabr_styles_to_combine) {
+    base_style <- style_inherit(base_style, s)
+  }
 
-  if (length(seperated_style_definition) <= 1) {
-    return(tab$style_catalogue[[seperated_style_definition]])
-  }
-  else{
-    # Otherwise build final style
-    previous_style <- style_key_parser(tab$style_catalogue[[seperated_style_definition[1]]])
-    for (base_style_index in 2:length(seperated_style_definition)) {
-      current_style <- style_key_parser(tab$style_catalogue[[seperated_style_definition[base_style_index]]])
-      previous_style <- cascade_style(previous_style, current_style)
-    }
-  }
-  return(convert_style_list_to_character_key(previous_style))
+  base_style
+
 }
 
-add_style_defintions_to_catalogue <- function(tab, style_definitions){
+style_inherit <- function(base_xltabr_style, new_xltabr_style) {
 
-  for (style_def in style_definitions) {
-    style_key <- build_style(tab, style_def)
+  new_style_list <- modifyList(base_xltabr_style$style_list, new_xltabr_style$style_list)
 
-    # Always log the key value pair in the style dictionary
-    if (!style_def %in% names(tab$style_catalogue)) {
-      tab$style_catalogue[[style_def]] <- style_key
-    }
+  new_xltabr_style$style_list <- new_style_list
+
+  # Take the greater of the row heights if at least one is not null
+  both_null <- all(is.null(c(base_xltabr_style$row_height, new_xltabr_style$row_height)))
+  if (!both_null) {
+    new_xltabr_style$row_height <- max(base_xltabr_style$row_height, new_xltabr_style$row_height)
   }
 
-  tab
+  # Inherit number format
+  if (is.null(new_xltabr_style$excel_num_format)) {
+    new_xltabr_style['excel_num_format'] <- list(base_xltabr_style$excel_num_format)
+  }
+
+  new_xltabr_style$s4style <- generate_s4_object_from_xltabr_style(new_xltabr_style)
+  new_xltabr_style$style_string <- paste(base_xltabr_style$style_string, new_xltabr_style$style_string, sep = "|")
+
+  new_xltabr_style
+
 }
 
+generate_s4_object_from_xltabr_style <- function(xltabr_style) {
 
-convert_style_object_to_list <- function(style, convert_to_S4 = FALSE){
-  if (convert_to_S4) {
+  style_properties <- names(xltabr_style$style_list)
 
-    if (typeof(style) == "character") {
-      style <- style_key_parser(style)
-    }
-
-    style_properties <- names(style)
-
-    if ("numFmt" %in% style_properties) {
-      style_properties <- style_properties[!("numFmt" == style_properties)]
-      out_style <- openxlsx::createStyle(numFmt = style[["numFmt"]])
-    } else{
-      out_style <- openxlsx::createStyle()
-    }
-
-    for (prop in style_properties) {
-      out_style[[prop]] <- style[[prop]]
-    }
-
-    return(out_style)
-
+  if (is.null(xltabr_style$excel_num_format)) {
+    excel_num_format <- "GENERAL"
   } else {
-    out_style <- style$as.list()
-
-    # For some reason fills do not export properly so workaround is added here
-    if (any(c("fillFg", "fillBg") %in% names(out_style))) {
-      out_style[["fill"]] <- list(fillFg = out_style[["fillFg"]], fillBg = out_style[["fillBg"]])
-      out_style[["fillFg"]] <- NULL
-      out_style[["fillBg"]] <- NULL
-    }
-    return(out_style)
+    excel_num_format <- xltabr_style$excel_num_format
   }
+
+  out_style <- openxlsx::createStyle(numFmt = excel_num_format)
+
+  for (prop in style_properties) {
+    out_style[[prop]] <- xltabr_style$style_list[[prop]]
+  }
+
+  out_style
 }
 
+create_style_if_not_exists <- function(tab, style_string) {
 
-add_styles_to_wb <- function(tab){
+  # If the style already exists
+  if (style_string %in% names(tab$style_catalogue)) {
+    return(tab)
+  }
 
-  # Get a table with a style for each cell
+  # Otherwise create it
+  new_xltabr_style <- style_string_parse_and_combine(tab$style_catalogue, style_string)
+  tab$style_catalogue[[style_string]] <- new_xltabr_style
+  tab
+
+}
+
+add_styles_to_wb <- function(tab) {
+
+  #For each distinct style, see if it already exists in the style catalogue, otherwise,
+  #create it, add it, and apply it
+
   full_table <- combine_all_styles(tab)
 
-  if (is.null(full_table)) stop("Please ensure add_from has appropriate values and a table has been added to tab")
+  unique_style_combinations <- unique(full_table$style_name)
 
-  # Bloody factors
-  full_table <- data.frame(lapply(full_table, as.character), stringsAsFactors = FALSE)
+  for (style_combination_string in unique_style_combinations) {
 
-  # Get a unique vector of style_name from full_table
-  unique_styles_definitions <- unique(full_table$style_name)
+    row_col_styles <- full_table[full_table$style_name == style_combination_string,]
 
-  # Add unique style_name vector to style catalogue
-  tab <- add_style_defintions_to_catalogue(tab, unique_styles_definitions)
-
-  # add the style_key to each style name in full table
-  full_table$style_key <- unlist(tab$style_catalogue[full_table$style_name])
-
-  # iterate over a unique list of style_keys for and apply them to the workbook for each row col
-  unique_style_keys <- unique(full_table$style_key)
-
-  for (sk in unique_style_keys) {
-    row_col_styles <- full_table[full_table$style_key == sk,]
+    #e.g. rows[1] cols[1] is the first cell this style will be applied to
     rows <- row_col_styles$row
     cols <- row_col_styles$col
 
-    created_style <- convert_style_object_to_list(sk, convert_to_S4 = TRUE)
+    tab <- create_style_if_not_exists(tab, style_combination_string)
+    xltabr_style <- tab$style_catalogue[[style_combination_string]]
 
-    # Add cell style
-    openxlsx::addStyle(tab$wb, tab$misc$ws_name, created_style, rows, cols)
+    openxlsx::addStyle(tab$wb, tab$misc$ws_name, xltabr_style$s4style, rows, cols)
 
-    # If this row's row height is greater than apply row height
-    created_style <- style_key_parser(sk)
-    existing_row_height <-  get_prop_or_return_null(tab$wb$rowHeights[[1]], as.character(rows[[1]]))
+    tab <- update_row_heights(tab, rows, xltabr_style)
 
-    if (is.null(existing_row_height)) {
-      existing_row_height <- 15
-    } else {
-      existing_row_height <- as.double(existing_row_height)
-    }
+  }
 
-    new_row_height <- get_prop_or_return_null(created_style, "row_height")
+  tab
 
-    if (not_null(new_row_height)) {
-      if (new_row_height > existing_row_height) {
-        openxlsx::setRowHeights(tab$wb, tab$misc$ws_name, rows, new_row_height)
-      }
+}
+
+update_row_heights <- function(tab, rows, xltabr_style) {
+  # Row heights are a bit tricky because we need to iterate through each row
+  # checking existing height and increasing height if existing height < new height
+  # Need to add sheet in here probably - presumably [[1]] is the sheet
+  worksheet_index <- which(tab$wb$sheet_names == tab$misc$ws_name)
+
+  for (row in unique(rows)) {
+
+    existing_row_height <-  get_prop_or_return_null(tab$wb$rowHeights[[worksheet_index]], as.character(row))
+    new_row_height <- xltabr_style$row_height
+
+    all_null <- all(is.null(c(existing_row_height, new_row_height)))
+
+    if (!all_null) {
+      new_row_height <- max(existing_row_height, new_row_height)
+      openxlsx::setRowHeights(tab$wb, tab$misc$ws_name, row, new_row_height)
     }
   }
 
   tab
-}
 
-# Not used in package - used for debug
-compare_style_lists <- function(a, b){
-  a_names <- sort(names(a))
-  b_names <- sort(names(b))
-
-  if (length(a_names) != length(b_names)) return(FALSE)
-  if (!all(a_names == b_names)) return(FALSE)
-
-  final_check <- TRUE
-  for (prop in a_names) {
-    if (!identical(a[prop], b[prop])) {
-      final_check <- FALSE
-      break;
-    }
-  }
-  return(final_check)
 }
