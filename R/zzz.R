1568dd15d725c128c446bfc899e3e7800d16afc8
Karik Isichei <karik.isichei@gmail.com>
2017-11-02 11:10:56 +0000

    Fixing issue where xltabr_default_options values are stored in lazy load. Meaning that the call to system.file is not ran for those using Mac OS or windows. xltabr_default_options are now in scope of .onload and should now not be stored in xltabr.rdb.

--- R/zzz.R
+++ R/zzz.R
@@ -1,13 +1,14 @@
-xltabr_default_options <- list(
-  xltabr.style.path = system.file("extdata", "styles.xlsx", package = "xltabr"),
-  xltabr.cell.format.path = system.file("extdata", "style_to_excel_number_format.csv", package = "xltabr"),
-  xltabr.number.format.path = system.file("extdata", "number_format_defaults.csv", package = "xltabr"),
-  xltabr.style.override.path = NULL)
-
 .onLoad <- function(libname, pkgname) {
+
+  xltabr_default_options <- list(
+    xltabr.style.path = system.file("extdata", "styles.xlsx", package = "xltabr"),
+    xltabr.cell.format.path = system.file("extdata", "style_to_excel_number_format.csv", package = "xltabr"),
+    xltabr.number.format.path = system.file("extdata", "number_format_defaults.csv", package = "xltabr"),
+    xltabr.style.override.path = NULL)
+
   op <- options()
   toset <- !(names(xltabr_default_options) %in% names(op))
-  if(any(toset)) options(xltabr_default_options[toset])
+  if (any(toset)) options(xltabr_default_options[toset])
 
   invisible()
 }
